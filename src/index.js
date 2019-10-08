9923082a64850cbe1254a1291bdcf430e1454872
Yuri-9 <vashkevichyp@gmail.com>
2019-10-09 00:08:26 +0300

    feat: add solution for morsr-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    function returnCharMorse(charBinary){
+        let charMorseFun = "";
+        for (let i=0; i<charBinary.length; i=i+2) {               
+            let twoLetters = charBinary[i]+charBinary[i+1];
+            if (twoLetters == "00") {
+                charMorseFun = charMorseFun + "";
+            } else if (twoLetters == "10") {
+                charMorseFun = charMorseFun + ".";
+            } else if (twoLetters == "11") {
+                charMorseFun = charMorseFun + "-";
+            } else {
+                charMorseFun = charMorseFun + "*";
+             }
+        }
+        return charMorseFun;       
+    }
+
+    let arrayCharBinary = [];
+    let charMorse="";
+    let sentence = "";
+    let key;
+
+    for (let i=0; i<expr.length; i=i+10) {
+        let char = expr.slice(i, i+10);
+        arrayCharBinary.push(char);
+    }  
+
+    for (let i=0; i<arrayCharBinary.length; i++) {
+        key = returnCharMorse(arrayCharBinary[i]); 
+        if (key == '*****') {
+            sentence = sentence + ' ';
+        } else {
+            sentence = sentence+MORSE_TABLE[key];        
+            charMorse = "";
+        }
+    }
+    return sentence;
 }
 
 module.exports = {
